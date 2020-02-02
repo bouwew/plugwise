99e380b929b61f46db24a8626391db93f1d08e0f
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2020-02-02 11:15:21 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='plugwise',
+    version='0.0.1',
+    description='Plugwise API to use in conjunction with Home Assistant.',
+    long_description='Plugwise API supporting Adam and Anna/Smile (firmware 3.x and up) to use in conjunction with Home Assistant, but it can also be used without Home Assistant.',
+    keywords='Home Assistant HA Adam Anna Smile Lisa Tom Floor Plugwise',
+    url='https://github.com/bouwew/plugwise',
+    author='bouwew',
+    author_email='bouwe.s.westerdijk@gmail.com',
+    license='MIT',
+    packages=['plugwise'],
+    install_requires=['requests','datetime','pytz'],
+    zip_safe=False
+)
