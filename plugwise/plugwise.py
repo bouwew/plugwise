1fafb6634139f00793455b9de23f930c9e9cf98c
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-02-22 17:50:34 +0100

    Another try

--- plugwise/plugwise.py
+++ plugwise/plugwise.py
@@ -568,7 +568,7 @@ class Plugwise:
     def _set_temp(self, root, loc_id, loc_type, temperature):
         """Sends a temperature-set request, helper function."""
         uri = self.__get_temperature_uri(root, loc_id, loc_type)
-        if temperature:
+        if temperature is not None:
             temperature = str(temperature)
 
         xml = requests.put(
