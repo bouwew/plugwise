7526b61ec9a31cc6e0e4286fe02d61cd18ee23bc
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-02-23 11:10:43 +0100

    Fix _set_preset function

--- plugwise/plugwise.py
+++ plugwise/plugwise.py
@@ -525,45 +525,47 @@ class Plugwise:
     def _set_preset(self, root, loc_id, loc_type, preset):
         """Sets the preset, helper function."""
         location_ids = []
-        locations = root.findall('.//location')
-        for location in locations:
-            locator = ("appliance[type='" + loc_type + "']/location")
-            if root.find(locator) is not None:
-                location_ids.append(root.find(locator).attrib['id'])
-        for location_id in location_ids:
-            if location_id == loc_id:
-                locations_root = self.get_locations()
-                current_location = locations_root.find("location[@id='" + location_id + "']")
-                location_name = current_location.find('name').text
-                location_type = current_location.find('type').text
+        appliances = root.findall('.//appliance')
+        for appliance in appliances:
+            if appliance.find('type') is not None:
+                appliance_type = appliance.find('type').text
+                if appliance_type == loc_type:
+                    for location in appliance.iter('location'):
+                        if location.attrib is not None:
+                            location_id = location.attrib['id']
+                            if location_id == loc_id:
+                                locations_root = self.get_locations()
+                                current_location = locations_root.find("location[@id='" + location_id + "']")
+                                location_name = current_location.find('name').text
+                                location_type = current_location.find('type').text
 
-                xml = requests.put(
-                        self._endpoint
-                        + LOCATIONS
-                        + ";id="
-                        + location_id,
-                        auth=(self._username, self._password),
-                        data="<locations>"
-                        + '<location id="'
-                        + location_id
-                        + '">'
-                        + "<name>"
-                        + location_name
-                        + "</name>"
-                        + "<type>"
-                        + location_type
-                        + "</type>"
-                        + "<preset>"
-                        + preset
-                        + "</preset>"
-                        + "</location>"
-                        + "</locations>",
-                        headers={"Content-Type": "text/xml"},
-                        timeout=10,
-                    )
-                if xml.status_code != requests.codes.ok: # pylint: disable=no-member
-                    raise CouldNotSetPresetException("Could not set the given preset: " + xml.text)
-                return xml.text
+                                xml = requests.put(
+                                        self._endpoint
+                                        + LOCATIONS
+                                        + ";id="
+                                        + location_id,
+                                        auth=(self._username, self._password),
+                                        data="<locations>"
+                                        + '<location id="'
+                                        + location_id
+                                        + '">'
+                                        + "<name>"
+                                        + location_name
+                                        + "</name>"
+                                        + "<type>"
+                                        + location_type
+                                        + "</type>"
+                                        + "<preset>"
+                                        + preset
+                                        + "</preset>"
+                                        + "</location>"
+                                        + "</locations>",
+                                        headers={"Content-Type": "text/xml"},
+                                        timeout=10,
+                                    )
+                                if xml.status_code != requests.codes.ok: # pylint: disable=no-member
+                                    raise CouldNotSetPresetException("Could not set the given preset: " + xml.text)
+                                return xml.text
 
     def _set_temp(self, root, loc_id, loc_type, temperature):
         """Sends a temperature-set request, helper function."""
